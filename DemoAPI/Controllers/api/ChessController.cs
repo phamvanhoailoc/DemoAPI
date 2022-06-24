517c3b633faa69e69021eeba9601eedf10e34316
Admin <phamvanhoailoc0507@gmail.com>
2022-06-24 12:55:02 +0700

    hiện quân cờ sỹ

---
+++ DemoAPI/Controllers/api/ChessController.cs
@@ -0,0 +1,69 @@
+﻿using DemoAPI.Models;
+using Lib.Entities;
+using Lib.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoAPI.Controllers.api
+{
+    public class ChessController: Controller
+    {
+        ChessService chessService = new ChessService();
+        
+        [Route("api/chess/insertroom")]
+        [HttpPost]
+        public ActionResult insertroom(RoomModel rmodel)
+        {
+            Room r = new Room();
+            r.Id = Guid.NewGuid();
+            r.Name = rmodel.RoomName;
+            chessService.insertRoom(r);
+            return
+            Json(new
+            {
+                message = "success",
+               /// data = stList //_dbContext.Student.OrderBy(s=>s.Id).Skip(2).Take(3).ToList() //Where(s=>s.Id == Guid.Parse(id)).FirstOrDefault()
+            }, JsonRequestBehavior.AllowGet);
+        }
+        [Route("api/chess/getrooms")]
+        [HttpGet]
+        public ActionResult getallroom()
+        {
+            List<Room> roomList = chessService.getAllRoom();
+            return
+            Json(new
+            {
+                message = "success",
+                data = roomList //_dbContext.Student.OrderBy(s=>s.Id).Skip(2).Take(3).ToList() //Where(s=>s.Id == Guid.Parse(id)).FirstOrDefault()
+            }, JsonRequestBehavior.AllowGet);
+        }
+        [Route("api/chess/getchessnode")]
+        [HttpPost]
+        public ActionResult getAllNode(List<MoveModel> movelist)
+        {
+            string chessJson = System.IO.File.ReadAllText(Server.MapPath("/Data/ChessJson.txt"));
+            System.Web.Script.Serialization.JavaScriptSerializer js = new System.Web.Script.Serialization.JavaScriptSerializer();
+            List<ChessNode> chessnode = js.Deserialize<List<ChessNode>>(chessJson);
+            return Json(new
+            {
+                message = "success",
+                chessnode = chessnode
+            }, JsonRequestBehavior.AllowGet);
+        }
+        [Route("api/chess/movenode")]
+        [HttpPost]
+        public ActionResult getMoveNode(List<MoveModel> movelist)
+        {
+            System.Web.Script.Serialization.JavaScriptSerializer js = new System.Web.Script.Serialization.JavaScriptSerializer();
+            Requestlog.PostToClient(js.Serialize(movelist));
+            return Json(new
+            {
+                message = "success"
+
+            }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
